2fd883a51ba9f19111d80af5cec873d56cea0485
Mingwei Tian <mwtian@live.com>
2014-03-13 11:19:08 -0700

    New FSharp 3.1 MVC5 app in .NET 4.5.1.

---
+++ FSharp31MVCTest/Controllers/HomeController.fs
@@ -0,0 +1,13 @@
+namespace FSharp31MVCTest.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Web
+open System.Web.Mvc
+open System.Web.Mvc.Ajax
+
+type HomeController() =
+    inherit Controller()
+    member this.Index () = this.View()
+
---
+++ FSharp31MVCTest/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Inherits="FSharp31MVCTest.Global" %>
